f279084e4820ffca4956758a50f405578eb15b46
Geoffrey.C <cycjimmy@gmail.com>
2022-03-16 14:55:43 +0800

    fix: compatible with swiper7+

--- src/root/templates.js
+++ src/root/templates.js
@@ -6,7 +6,7 @@
 // eslint-disable-next-line import/prefer-default-export
 export const rootTemplate = ({ style }) => `
 <div class="${style.root}">
-  <div class="swiper-container ${style.swiperContainer}">
+  <div class="swiper swiper-container ${style.swiperContainer}">
     <div class="swiper-wrapper ${style.swiperWrapper}"></div>
   </div>
 </div>
